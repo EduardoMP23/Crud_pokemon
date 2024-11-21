30f28490bacfbb6df4d592a6a59111bca21a6457
EduardoMP23 <eduardopereira2002@gmail.com>
2024-11-21 16:01:08 -0300

    por garantia

---
+++ resources/views/layouts/blade.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="container bg-gradient-to-l from-purple-600 to-red-700 h-screen  ">
+        @yield('content')
+    </div>    
+
+
+
+</body>
+</html>
